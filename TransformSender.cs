e2eae727713a4697361922d23ac71bc021ab0f13
Emiel Harmsen <emielch@gmail.com>
2017-11-01 17:49:14 +0100

    init

---
+++ TransformSender.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace oi.plugin.transformsend {
+
+    public class TransformSender : MonoBehaviour {
+        public bool continuousSend = true;
+        private bool continuousSendMeasure = false;
+        private bool measuring = false;
+
+        public float intervalSec = 2;
+        private float timer = 0;
+        public int transformID;
+        public float measureTime = 2;
+
+        private List<Vector3> positions = new List<Vector3>();
+        private List<Quaternion> rotations = new List<Quaternion>();
+
+        private Vector3 position = new Vector3(0, 0, 0);
+        private Quaternion rotation = new Quaternion(0, 0, 0, 0);
+
+        public IMPRESS_UDPClient transformSocket;
+
+        void Update() {
+            timer += Time.deltaTime;
+
+            if (measuring) {
+                continuousSendMeasure = false;
+                positions.Add(transform.position);
+                rotations.Add(transform.rotation);
+
+                if (timer >= measureTime) {
+                    measuring = false;
+                    continuousSendMeasure = true;
+
+                    position = FilteredMeanPos(positions);
+                    rotation = FilteredMeanRot(rotations);
+                }
+            }
+
+            if (continuousSend) {
+                if (timer >= intervalSec) {
+                    timer = 0;
+                    byte[] serialized = TransformSerializer.Serialize(transform, transformID);
+                    transformSocket.SendData(serialized);
+                }
+            }
+
+            if (continuousSendMeasure) {
+                if (timer >= intervalSec) {
+                    timer = 0;
+                    byte[] serialized = TransformSerializer.Serialize(position, rotation, transformID);
+                    transformSocket.SendData(serialized);
+                }
+            }
+
+        }
+
+        public bool IsMeasuring() {
+            return measuring;
+        }
+
+        public void MeasureAndSend() {
+            continuousSend = false;
+            measuring = true;
+            timer = 0;
+            positions.Clear();
+            rotations.Clear();
+            position = new Vector3(0, 0, 0);
+            rotation = new Quaternion(0, 0, 0, 0);
+        }
+
+
+        public Vector3 FilteredMeanPos(List<Vector3> poss) {
+            Vector3 mean = AvgPos(poss);
+            float stdev = StdevPos(poss);
+            List<Vector3> filtPoss = new List<Vector3>();
+
+            foreach (Vector3 pos in poss) {
+                float dist = Mathf.Abs(Vector3.Distance(mean, pos));
+                if (dist < stdev) filtPoss.Add(pos);
+            }
+
+            return AvgPos(filtPoss);
+        }
+
+        public Quaternion FilteredMeanRot(List<Quaternion> rots) {
+            Quaternion mean = AvgRot(rots);
+            float stdev = StdevRot(rots);
+            List<Quaternion> filtRots = new List<Quaternion>();
+
+            foreach (Quaternion rot in rots) {
+                float dist = Mathf.Abs(Quaternion.Angle(mean, rot));
+                if (dist < stdev) filtRots.Add(rot);
+            }
+
+            return AvgRot(filtRots);
+        }
+
+
+
+        float StdevRot(List<Quaternion> rots) {
+            Quaternion mean = AvgRot(rots);
+
+            float variance = 0;
+            foreach (Quaternion rot in rots) {
+                variance += Mathf.Pow(Quaternion.Angle(mean, rot), 2);
+            }
+            variance /= rots.Count;
+            return Mathf.Sqrt(variance);
+        }
+
+        float StdevPos(List<Vector3> poss) {
+            Vector3 mean = AvgPos(poss);
+
+            float variance = 0;
+            foreach (Vector3 pos in poss) {
+                variance += Mathf.Pow(Vector3.Distance(mean, pos), 2);
+            }
+            variance /= poss.Count;
+            return Mathf.Sqrt(variance);
+        }
+
+        public Quaternion AvgRot(List<Quaternion> rots) {
+            int sampleAm = 0;
+            Quaternion avg = new Quaternion();
+
+            foreach (Quaternion rot in rots) {
+                sampleAm++;
+                avg = Quaternion.Slerp(avg, rot, 1f / sampleAm);
+            }
+
+            return avg;
+        }
+
+        public Vector3 AvgPos(List<Vector3> poss) {
+            Vector3 avg = new Vector3();
+
+            foreach (Vector3 pos in poss) {
+                avg += pos / poss.Count;
+            }
+
+            return avg;
+        }
+    }
+
+}
---
+++ TransformSerializer.cs
@@ -0,0 +1,141 @@
+﻿using SysDiag = System.Diagnostics;
+using System.IO;
+using UnityEngine;
+
+namespace oi.plugin.transformsend {
+
+    public static class TransformSerializer {
+        /// <summary>
+        /// The trans header consists of one 32 bit integers.
+        /// </summary>
+        private static int HeaderSize = sizeof(int) * 1;
+
+
+        public static byte[] Serialize(Transform trans, int id) {
+            byte[] data = null;
+
+            using (MemoryStream stream = new MemoryStream()) {
+                using (BinaryWriter writer = new BinaryWriter(stream)) {
+                    WriteTrans(writer, trans.position, trans.rotation, id);
+
+                    stream.Position = 0;
+                    data = new byte[stream.Length];
+                    stream.Read(data, 0, data.Length);
+                }
+            }
+
+            return data;
+        }
+
+        public static byte[] Serialize(Vector3 pos, Quaternion rot, int id) {
+            byte[] data = null;
+
+            using (MemoryStream stream = new MemoryStream()) {
+                using (BinaryWriter writer = new BinaryWriter(stream)) {
+                    WriteTrans(writer, pos, rot, id);
+
+                    stream.Position = 0;
+                    data = new byte[stream.Length];
+                    stream.Read(data, 0, data.Length);
+                }
+            }
+
+            return data;
+        }
+
+
+        public static void Deserialize(byte[] data, out int transformID, out Vector3 pos, out Quaternion rot) {
+            pos = new Vector3();
+            rot = new Quaternion();
+            transformID = -1;
+
+            using (MemoryStream stream = new MemoryStream(data)) {
+                using (BinaryReader reader = new BinaryReader(stream)) {
+                    while (reader.BaseStream.Length - reader.BaseStream.Position >= HeaderSize) {
+                        ReadTrans(reader, out transformID, out pos, out rot);
+                    }
+                }
+            }
+        }
+
+
+        private static void WriteTrans(BinaryWriter writer, Vector3 pos, Quaternion rot, int id) {
+            SysDiag.Debug.Assert(writer != null);
+
+            WriteTransHeader(writer, id);
+            WritePosition(writer, pos);
+            WriteRotation(writer, rot);
+        }
+
+
+        private static void ReadTrans(BinaryReader reader, out int transformID, out Vector3 pos, out Quaternion rot) {
+            SysDiag.Debug.Assert(reader != null);
+
+            int packetID = -1;
+            transformID = -1;
+
+            pos = new Vector3();
+            rot = new Quaternion();
+
+            ReadHeader(reader, out packetID, out transformID);
+            if (packetID != 2) return;
+
+            pos = ReadPosition(reader);
+            rot = ReadRotation(reader);
+        }
+
+
+        private static void WriteTransHeader(BinaryWriter writer, int id) {
+            SysDiag.Debug.Assert(writer != null);
+
+            writer.Write(2); // '2' announces transform packet
+            writer.Write(id);
+        }
+
+
+        private static void ReadHeader(BinaryReader reader, out int packetID, out int transformID) {
+            SysDiag.Debug.Assert(reader != null);
+
+            packetID = reader.ReadInt32();
+            transformID = reader.ReadInt32();
+        }
+
+
+        private static void WritePosition(BinaryWriter writer, Vector3 location) {
+            SysDiag.Debug.Assert(writer != null);
+
+            writer.Write(location.x);
+            writer.Write(location.y);
+            writer.Write(location.z);
+        }
+
+
+        private static Vector3 ReadPosition(BinaryReader reader) {
+            SysDiag.Debug.Assert(reader != null);
+
+            Vector3 position = new Vector3(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
+
+            return position;
+        }
+
+ 
+        private static void WriteRotation(BinaryWriter writer, Quaternion rotation) {
+            SysDiag.Debug.Assert(writer != null);
+
+            writer.Write(rotation.x);
+            writer.Write(rotation.y);
+            writer.Write(rotation.z);
+            writer.Write(rotation.w);
+        }
+
+
+        private static Quaternion ReadRotation(BinaryReader reader) {
+            SysDiag.Debug.Assert(reader != null);
+
+            Quaternion rotation = new Quaternion(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
+
+            return rotation;
+        }
+    }
+
+}
